14d458349718f1ca58e4dccd480124e35669d133
Nathnael Yewondwosen <50904279+nattigy@users.noreply.github.com>
2021-07-23 12:33:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ const app = express();
 
 const server = new ApolloServer({
   schema,
-  playground: true,
-  introspection: true,
+  playground: false,
+  introspection: false,
   tracing: true,
   path: "/",
 });
